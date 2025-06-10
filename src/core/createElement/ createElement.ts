4e8b8b3e98c987346245e06a7c6394260dfe1046
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-10 22:26:23 +0900

    ♻️ Wip: createElement 함수 타입 리팩토링
    기존의 Props 타입을 인덱스 시그니처로 선언한 뒤 반환값의 타입으로 선언되지 않은 Children타입을 사용해서 타입 에러 발생함. ->. 실제로 props 으로 올수 있는 타입을 모두 선언해 리팩토링 완료.

--- src/core/createElement/ createElement.ts
+++ src/core/createElement/ createElement.ts
@@ -1,15 +1,62 @@
-export default function createElement(type, props, ...children) {
-  const { ref, key, ...rest } = props || null; // 불변성 유지
-  const normalizedChildren = Array.isArray(children)
-    ? children.flat(Infinity)
+type Child = VNode | string | number | null;
+type Children = Child[] | null;
+
+interface HTMLAttributes {
+  // HTML 기본 속성
+  className?: string;
+  id?: string;
+  // 이벤트 핸들러
+  onClick?: (event: MouseEvent) => void;
+  onChange?: (event: Event) => void;
+  onKeyDown?: (event: KeyboardEvent) => void;
+  onKeyUp?: (event: KeyboardEvent) => void;
+  onKeyPress?: (event: KeyboardEvent) => void;
+  onFocus?: (event: FocusEvent) => void;
+  onBlur?: (event: FocusEvent) => void;
+  onInput?: (event: Event) => void;
+  onMouseEnter?: (event: MouseEvent) => void;
+  onMouseLeave?: (event: MouseEvent) => void;
+  // 중요한 props들 ...?
+  ref?: any;
+  key?: string | number;
+}
+
+type VNodeProps = HTMLAttributes & { children: Children };
+
+interface VNode {
+  type: string | Function;
+  key: string | number | null;
+  ref: any;
+  props: VNodeProps;
+}
+
+export default function createElement(
+  type: string | Function,
+  props: HTMLAttributes | null,
+  ...children: (Child | Child[])[]
+): Readonly<VNode> {
+  const { ref, key, ...rest } = props ?? {};
+  const normalizedChildren: Children = Array.isArray(children)
+    ? (children.flat(Infinity) as Child[])
     : null;
-  return Object.freeze({
+
+  const normalizedKey =
+    typeof key === "string" || typeof key === "number" ? key : null;
+
+  const rawChildren: Children =
+    normalizedChildren?.length === 0 ? null : normalizedChildren;
+
+  const rawResult: VNode = {
     type,
-    key: key ?? null,
+    key: normalizedKey,
     ref: ref ?? null,
     props: {
       ...rest,
-      children: normalizedChildren.length === 0 ? null : normalizedChildren,
+      children: rawChildren,
     },
-  });
+  };
+
+  const result: Readonly<VNode> = Object.freeze(rawResult);
+
+  return result;
 }
