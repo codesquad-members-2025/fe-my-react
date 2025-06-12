65ccf11a4e1e651b100233c556cadf6c453c3640
Nago730 <jqk1797@gmail.com>
2025-06-12 18:54:52 +0900

    refactor: Vite 설정에서 테스트 관련 구성 제거 및 오탈자 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [tsconfigPaths()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['./tests/helpers/testSetup.ts'],
-	},
 });
