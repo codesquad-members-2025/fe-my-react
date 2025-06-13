1dda88f0cf61b6f4dd0fe349287a4741f3ffcfd3
Nago730 <jqk1797@gmail.com>
2025-06-13 18:59:36 +0900

    feat: jsx, jsxs 모듈 구현 및 테스트 코드 구현

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,9 +1,44 @@
 import { createElement, createFragment } from './core';
 
+type VNode = {
+	type: any;
+	props: any;
+	children: any[];
+	key?: any;
+	ref?: any;
+};
+
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export function jsx(type: any, props: any, key: string | null = null) {
-	return createElement(type, props, key);
+function jsx(type: any, props: any, key: string | null = null) {
+	const { children, ref, ...restProps } = props;
+
+	const vnode: VNode = {
+		type,
+		props: restProps,
+		children: children ? [children] : children,
+	};
+	if (key) vnode.key = key;
+	if (ref) vnode.ref = ref;
+
+	return createElement(vnode);
 }
 
-export const jsxs = jsx;
-export const Fragment = createFragment;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+function jsxs(type: any, props: any, key: string | null = null) {
+	const { children, ref, ...restProps } = props;
+
+	const vnode: VNode = {
+		type,
+		props: restProps,
+		children,
+	};
+	if (key) vnode.key = key;
+	if (ref) vnode.ref = ref;
+
+	return createElement(vnode);
+}
+
+const Fragment = createFragment;
+
+// 필수 export 모듈. 제외 불가
+export { jsx, jsxs, Fragment };
