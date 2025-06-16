e4772f8c6cf9d982f3e6e42f49cffa62d70cb562
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-16 19:41:19 +0900

    ✨ eventHandler 파일 구현 완료
    각 노드에 사용되는 이벤트를 중복되지 않도록 new Set을 이용하여 등록 하는 로직 구현 완료.

---
+++ src/core/render/eventHandelr.ts
@@ -0,0 +1,10 @@
+// 이벤트 등록 전용 맵
+export const eventList = new Set();
+
+/**
+ *
+ * @param action 각 노드에 발생하는 이벤트를 입력하여 등록합니다.
+ */
+export function registerEvent(action: string): void {
+  eventList.add(action);
+}
