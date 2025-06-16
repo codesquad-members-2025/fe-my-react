3670be4a672eda30fe3a65a0edea5e8ad3b12fc8
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-16 11:37:39 +0900

    ✨ 개발환경 구성완료
    ts로 구현한 프레임워크 코드와 테스트할 dev 환경 분리를 고려해 config 파일이용해 개발환경 분리 완료.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,40 @@
 // vite.config.ts
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // dev 서버의 루트 디렉토리를 jsx-demo로 지정
+  /*
+   * dev 서버의 루트 디렉토리를 jsx-demo로 지정
+   * 실제 테스트할 dev 환경이랑 프레임워크 코드랑 분리하는 작업.
+   */
+
   root: path.resolve(__dirname, "jsx-demo"),
 
   plugins: [
+    // root 의 경로안에 있는 tsconfig 가 아닌 외부 경로의 config 파일을 참조하기 위해 직접 경로 선언
+    tsconfigPaths({ projects: [path.resolve(__dirname, "tsconfig.json")] }),
     react({
       // classic JSX 런타임 사용
       jsxRuntime: "classic",
       // babel.config.js의 설정을 사용
     }),
   ],
+  esbuild: {
+    // .jsx 파일 전용 로더
 
-  resolve: {
-    alias: {
-      // src/core 안의 모듈을 쉽게 import
-      "@my-react": path.resolve(__dirname, "src/core"),
-      // jsx-demo 폴더의 모듈을 쉽게 import
-      "@jsx-demo": path.resolve(__dirname, "jsx-demo"),
-    },
+    loader: "jsx",
+
+    include: /src\/.*\.jsx$/,
+
+    // Classic 런타임 프래그마 지정
+
+    jsxFactory: "MyReact.createElement",
+
+    jsxFragment: "MyReact.Fragment",
+
+    jsxInject: `import MyReact from '@core';`,
   },
 
   build: {
@@ -33,6 +45,8 @@ export default defineConfig({
   },
 
   server: {
+    //루트(jsx-demo) 밖의 src 폴더를 허용해야 브라우저가 프레임워크 코드를 불러올 수 있음
+    fs: { allow: [path.resolve(__dirname, "src")] },
     port: 3000,
     // 자동으로 브라우저 열기
     open: true,
