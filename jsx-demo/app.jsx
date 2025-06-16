124a2f47fddbd04032b49248a46b4c650135f7c9
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-16 11:55:13 +0900

    🐛 별칭 적용 범위 수정
    기존의 tsconfig 파일에서 적용범위를 "src" 로만 지정해서 테스트 환경인 "jsx-demo"의 하위 경로의 파일에는 별칭이 지정 되지 않는 오류가 있었음. 이렇게 되면 jsxInject의 옵션에서 주입되는 경로인 별칭을 인식하지 못하는 문제가 있었음.
    그래서 tsconfig 의 include 옵션을  ["src", "jsx-demo"] 와 같이 확대하여 수정 완료.

--- jsx-demo/app.jsx
+++ jsx-demo/app.jsx
@@ -1,12 +1,14 @@
+import MyReact from "@/core";
+
 function App() {
-    return (
-        <div>
-            <h1>JSX Demo</h1>
-            <p>이것은 JSX 데모입니다.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>JSX Demo</h1>
+      <p>이것은 JSX 데모입니다.</p>
+    </div>
+  );
 }
 
 // DOM에 렌더링
-const root = document.getElementById('root');
-root.innerHTML = App(); //render 함수로 수정 필요
+const root = document.getElementById("root");
+render(<App />, root);
