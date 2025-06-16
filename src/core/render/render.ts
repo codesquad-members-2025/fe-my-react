4af9052e46f2775628501faf09325fed2293060c
Nago730 <jqk1797@gmail.com>
2025-06-16 12:56:11 +0900

    feat: render 함수 작성

---
+++ src/core/render/render.ts
@@ -0,0 +1,33 @@
+import { FRAGMENT, TEXT_ELEMENT } from './constants';
+
+export function render(vnode: any, container: Element) {
+	// 함수형 컴포넌트 처리
+	if (typeof vnode.type === 'function') {
+		const componentVNode = vnode.type(vnode.props); // 함수 호출로 자식 VNode 반환
+		return render(componentVNode, container); // 재귀 렌더링
+	}
+
+	let dom: Node;
+
+	if (vnode.type === TEXT_ELEMENT) {
+		dom = document.createTextNode(vnode.props.nodeValue);
+	} else if (vnode.type === FRAGMENT) {
+		dom = document.createDocumentFragment();
+	} else {
+		dom = document.createElement(vnode.type);
+	}
+
+	for (const [key, value] of Object.entries(vnode.props ?? {})) {
+		if (key !== 'children') {
+			(dom as any)[key] = value;
+		}
+	}
+
+	if (vnode.children) {
+		for (const child of vnode.children) {
+			render(child, dom as Element);
+		}
+	}
+
+	container.appendChild(dom);
+}
