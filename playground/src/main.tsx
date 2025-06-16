b30c60d3ce09d621cc0d89727812593e73187b5a
Nago730 <jqk1797@gmail.com>
2025-06-16 11:57:16 +0900

    refactor: 프로젝트 재시작을 위한 코드 삭제

--- playground/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from '@my-react/core/render';
-import App from './App';
-
-const root = document.getElementById('root');
-if (root) render(<App />, root);
--- src/core/createElement/createFragment.ts
+++
@@ -1,7 +0,0 @@
-import { createElement } from './createElement';
-import { FRAGMENT } from './createElement.constants';
-import type { VNode } from './createElement.types';
-
-export function createFragment(props: { children: (VNode | string)[] }): VNode {
-	return createElement(FRAGMENT, props);
-}
