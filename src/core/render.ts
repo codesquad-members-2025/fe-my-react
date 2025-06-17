46b9813a3ca2cbebced978bba4f9b0bf597ff1f4
Nago730 <jqk1797@gmail.com>
2025-06-17 18:12:45 +0900

    refactor: render 함수가 VNode를 렌더링하던 방식에서 Fiber를 렌더링하도록 변경

--- src/core/render.ts
+++ src/core/render.ts
@@ -1,37 +1,85 @@
-import type { FragmentVNode, TextVNode, VNode } from '../types/base.types';
+import { buildFiberTree } from '@src/runtime/fiber';
+import type {
+	Fiber,
+	FragmentVNode,
+	TextVNode,
+	VNode,
+} from '../types/base.types';
 import { applyRef } from './applyRef';
 import { FRAGMENT, TEXT_ELEMENT } from './constants';
+
+function createRootFiber(vnode: VNode): Fiber {
+	const rootFiber: Fiber = {
+		type: vnode.type,
+		props: vnode.props,
+		parent: null,
+		child: null,
+		sibling: null,
+		memoizedState: [],
+		hookIndex: 0,
+	};
+
+	if (vnode.key) rootFiber.key = vnode.key;
+	if (vnode.ref) rootFiber.ref = vnode.ref;
+
+	return rootFiber;
+}
+
+export function createDom(fiber: Fiber): Node | null {
+	const { type, props, ref } = fiber;
+
+	let dom: Node;
+
+	if (type === TEXT_ELEMENT) dom = document.createTextNode(props.nodeValue);
+	else if (type === FRAGMENT) dom = document.createDocumentFragment();
+	else if (typeof type === 'string') {
+		dom = document.createElement(type);
+
+		for (const [key, value] of Object.entries(props)) {
+			if (key === 'children') continue;
+			(dom as any)[key] = value;
+		}
+	} else {
+		// 예상되지 않는 상황이므로 무시
+		return null;
+	}
+
+	if (ref) applyRef(dom, ref);
+
+	return dom;
+}
+
+function commitToVirtualContainer(
+	fiber: Fiber,
+	container: DocumentFragment | Element,
+) {
+	const dom = createDom(fiber);
+	if (dom) container.appendChild(dom);
+
+	if (fiber.child && dom)
+		commitToVirtualContainer(fiber.child, dom as DocumentFragment | Element);
+	if (fiber.sibling) commitToVirtualContainer(fiber.sibling, container);
+}
 
 export function render(
 	vnode: VNode | TextVNode | FragmentVNode,
 	container: Element,
 ): void {
-	if (typeof vnode.type === 'function') {
-		const componentVNode = (vnode.type as Function)(vnode.props);
-		return render(componentVNode, container);
-	}
+	const rootVNode =
+		typeof vnode.type === 'function'
+			? (vnode.type as Function)(vnode.props)
+			: vnode;
 
-	let dom: Node;
+	const rootFiber: Fiber = createRootFiber(rootVNode);
 
-	if (vnode.type === TEXT_ELEMENT) {
-		dom = document.createTextNode((vnode as TextVNode).props.nodeValue);
-	} else if (vnode.type === FRAGMENT) {
-		dom = document.createDocumentFragment();
-	} else {
-		dom = document.createElement(vnode.type as string);
-	}
+	const children = rootVNode.props.children ?? [];
 
-	for (const [key, value] of Object.entries(vnode.props ?? {})) {
-		if (key === 'children') {
-			for (const child of value) {
-				render(child, dom as Element);
-			}
-		} else {
-			(dom as any)[key] = value;
-		}
-	}
+	buildFiberTree(rootFiber, children);
 
-	applyRef(dom, vnode.ref);
+	const virtualContainer = document.createDocumentFragment();
 
-	container.appendChild(dom);
+	commitToVirtualContainer(rootFiber, virtualContainer);
+
+	container.innerHTML = '';
+	container.appendChild(virtualContainer);
 }
