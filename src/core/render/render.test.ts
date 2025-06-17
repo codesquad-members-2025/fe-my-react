ca2fc41656a4daaa2d6660e98a46dba4f3d51dbc
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-17 18:44:06 +0900

    🐛 잘못된 테스트 순서 수정
    render 함수 실행 전에 존재하지 않는 노드에 접근하는 잘못된 순서 수정 완료.

--- src/core/render/render.test.ts
+++ src/core/render/render.test.ts
@@ -24,10 +24,10 @@ describe("render 함수 + jsdom 환경", () => {
       createElement("h2", null, "헤더"),
       createElement("p", null, "내용 텍스트")
     );
-    const div = container.querySelector("div.box");
 
     //When
     render(vnode, container);
+    const div = container.querySelector("div.box");
 
     //Then
     expect(div).toBeTruthy();
@@ -41,12 +41,12 @@ describe("render 함수 + jsdom 환경", () => {
     // 예를 들어, 테스트 대상 함수에 전달할 mock 함수나 초기 데이터 등을 만듭니다.
     const clickHandler = vi.fn(); // // vi.fn()은 Vitest에서 제공하는 mock 함수로, 호출 여부나 횟수 등을 추적할 수 있습니다.
     const vnode = createElement("button", { onClick: clickHandler }, "클릭");
-    const button = container.querySelector("button")!;
 
     // When (수행 단계)
     // 실제로 테스트하려는 동작을 실행합니다.
     // 예를 들어, 함수를 호출하거나, 이벤트를 발생시키는 등의 작업을 합니다.
     render(vnode, container);
+    const button = container.querySelector("button")!;
     button.click();
 
     // Then (테스트검증 단계)
