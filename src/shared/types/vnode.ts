5612521c0cec96985caf759bde9d5c2164a09638
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-20 11:39:13 +0900

    ♻️ 타입 수정
    가독성 향상 위해 각 타입 선언해 조합하여 HookMetaData타입 수정완료.

--- src/shared/types/vnode.ts
+++ src/shared/types/vnode.ts
@@ -1,13 +1,20 @@
 import type { HTMLAttributes } from "./props";
+
 export type Child = VNode | string | number | null;
 export type Children = Child[] | null;
+type State = unknown;
+type SetState = (newValue: any) => void;
+type Pointer = number;
+export type HookMetaData = [State, SetState, Pointer];
+export type HookMetaDataArr = Array<HookMetaData>;
 export interface VNode {
   type: string | Function;
   key: string | number | null;
   ref: any;
   props: HTMLAttributes & { children: Children };
   hookMetaData?: {
-    hooks: (string | number | boolean)[];
+    //TODO:포인터를 굳이 넣을 필요가 있을까...?
+    hooks: HookMetaDataArr;
     pointer: number;
   };
 }
