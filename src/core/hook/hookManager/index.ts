841ac964f5b30b04e347c48b7cf79e1f79e584fd
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-20 11:32:22 +0900

    ✨ getCurrentHookData 헬퍼 함수 구현
    하나의 노드안에 여러 hook들이 있어도 vnode의 pointer필드를 통해 순서에 알맞는 hookData를 반환하는 함수 구현 완료.

--- src/core/hook/hookManager/index.ts
+++ src/core/hook/hookManager/index.ts
@@ -1,4 +1,4 @@
-import type { VNode } from "@/shared/types/vnode";
+import type { HookMetaData, VNode } from "@/shared/types/vnode";
 
 let vnodeStack: VNode[] = [];
 
@@ -18,19 +18,60 @@ export function popCurrentVNode() {
 export function getCurrentVNode(): VNode {
   return vnodeStack[vnodeStack.length - 1];
 }
+/**
+ *
+ * @returns [registerHookHelper, getVNode, isInit] 를 반환합니다.
+ *
+ * 위 3개의 헬퍼함수를 반환합니다.
+ */
+export function useHookManger(): [
+  registerHookHelper: (state: any, setterFn: (newState: any) => void) => void,
+  getVNode: () => VNode,
+  getCurrentHookData: () => HookMetaData,
+  isInit: () => boolean
+] {
+  const hookMetaData = getCurrentVNode().hookMetaData!;
+  const hooks: Array<[unknown, (newValue: any) => void, number]> =
+    hookMetaData.hooks as Array<[unknown, (newValue: any) => void, number]>;
 
-export function hookManger() {
-  const metaData = getCurrentVNode().hookMetaData!;
-  const hooks: (string | number | boolean)[] = metaData.hooks;
-
-  const registerStateHelper = (state: any): void => {
-    hooks[metaData.pointer] = state;
-    metaData.pointer++;
+  /**
+   *
+   * @param state hook의 상태
+   * @param setterFn 등록할 setter 함수
+   *
+   * hook의 상태,setter함수(클로저 이용),해당hook의 pointer를 등록 합니다.
+   */
+  const registerHookHelper = (
+    state: any,
+    setterFn: (newState: any) => void
+  ): void => {
+    // TODO: 그냥 push로 하면 안되는거야? 고민 해보기
+    hooks[hookMetaData.pointer] = [state, setterFn, hookMetaData.pointer];
+    hookMetaData.pointer++;
+    /**
+     * hooks.push([state, setterFn, hookMetaData.pointer])
+     */
   };
 
-  const getState = () => hooks[metaData.pointer];
+  const getCurrentHookData = (): HookMetaData => {
+    const currentHookData = hooks[hookMetaData.pointer];
+    hookMetaData.pointer++;
+    return currentHookData;
+  };
 
-  const isInit = (): boolean => !!hooks[metaData.pointer];
+  /**
+   *
+   * @returns 현재의 VNode를 가져올 수 있습니다.
+   */
+  const getVNode = () => getCurrentVNode();
 
-  return [registerStateHelper, getState, isInit];
+  /**
+   *
+   * @returns 마운트, 리렌더링 분기를 불리언 타입으로 반환 합니다.
+   *
+   * 초기 마운트 상황일 때는 true를 , 리렌더링 상황이면 hookMetaData.pointer필드에 값이 있으므로 false를 반환합니다.
+   */
+  const isInit = (): boolean => !hooks[hookMetaData.pointer];
+
+  return [registerHookHelper, getVNode, getCurrentHookData, isInit];
 }
