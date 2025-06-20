71d9b7b74462c31cd4506d88ac3494c25b85c68f
youngmin <dudals2963@tukrorea.ac.kr>
2025-06-20 20:39:39 +0900

    ♻️ state 관련 타입 수정.

--- src/shared/types/vnode.ts
+++ src/shared/types/vnode.ts
@@ -2,10 +2,9 @@ import type { HTMLAttributes } from "./props";
 
 export type Child = VNode | string | number | null;
 export type Children = Child[] | null;
-type State = unknown;
-type SetState = (newValue: any) => void;
-type Pointer = number;
-export type HookMetaData = [State, SetState, Pointer];
+export type State = unknown;
+export type SetState = (newValue: any) => void;
+export type HookMetaData = [State, SetState];
 export type HookMetaDataArr = Array<HookMetaData>;
 export interface VNode {
   type: string | Function;
@@ -13,7 +12,6 @@ export interface VNode {
   ref: any;
   props: HTMLAttributes & { children: Children };
   hookMetaData?: {
-    //TODO:포인터를 굳이 넣을 필요가 있을까...?
     hooks: HookMetaDataArr;
     pointer: number;
   };
